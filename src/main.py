dda4f85fafbcde4d668df53c6d042ee359fe1624
Jonas Holt Pedersen <201905172@post.au.dk>
2022-09-25 14:22:11 +0200

    Added stuff

--- src/main.py
+++ src/main.py
@@ -1,2 +1,65 @@
-import keras
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#from __future__ import print_function
+import tensorflow
+from tensorflow import keras
+from keras.datasets import mnist
+from keras import backend as K
+from matplotlib import pyplot as plt
+from fast_soft_sort.tf_ops import soft_rank, soft_sort
+def show_imgs(x_test, n=10):
+    sz = x_test.shape[1]
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        ax = plt.subplot(2, n, i+1)
+        plt.imshow(x_test[i].reshape(sz,sz))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
 
+num_classes = 10
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+# Pre-process inputs
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# Convert class indices to one-hot vectors
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+print('Examples of test images')
+show_imgs(x_test)
+
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Activation
+
+tensorflow.random.set_seed(0) # make weight initialization deterministic
+
+def custom_activation(x):
+    return soft_rank(x)
+
+# Model
+model = Sequential()
+model.add(Flatten(input_shape=input_shape)) # input_shape is (28, 28, 1)
+model.add(Dense(num_classes)) # num_classes is 10
+model.add(Activation('soft_rank'))
+
+model.summary()
