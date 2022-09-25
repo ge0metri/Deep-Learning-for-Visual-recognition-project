a37e83eb92bc0a264007e6da1108935ab49365c6
Jonas Holt Pedersen <201905172@post.au.dk>
2022-09-25 14:42:14 +0200

    Added stuff we might not need

--- src/main.py
+++ src/main.py
@@ -45,21 +45,34 @@ x_test /= 255
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-print('Examples of test images')
-show_imgs(x_test)
+#print('Examples of test images')
+#show_imgs(x_test)
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Activation
-
+import tensorflow_ranking as tfr
 tensorflow.random.set_seed(0) # make weight initialization deterministic
-
-def custom_activation(x):
-    return soft_rank(x)
 
 # Model
 model = Sequential()
 model.add(Flatten(input_shape=input_shape)) # input_shape is (28, 28, 1)
 model.add(Dense(num_classes)) # num_classes is 10
-model.add(Activation('soft_rank'))
 
-model.summary()
+#model.summary()
+
+batch_size = 128
+epochs = 10
+
+# Compile the model before training
+model.compile(optimizer='sgd',loss=tfr.keras.losses.MeanSquaredLoss(),metrics=['accuracy'])
+
+# Fit model
+history = model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1,
+            validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Validation loss:', score[0])
+print('Validation accuracy:', score[1])
